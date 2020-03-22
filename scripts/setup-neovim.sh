35bba8eef5c10558333648678b16bf97c1d436c0
vancanhuit <vancanhuit@gmail.com>
2020-03-22 12:29:10 +0700

    Seperate scripts

---
+++ scripts/setup-neovim.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo '----- Installing nvm for coc.vim plugin -----'
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+
+echo '----- Installing vim-plug -----'
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp ../dotfiles/init.vim ~/.config/nvim/
--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -12,15 +12,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo '----- Installing zsh autosuggestions plugin -----'
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo '----- Installing nvm for coc.vim plugin in neovim -----'
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
-
-
-echo '----- Installing plugin mananager for neovim -----'
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+cp ../dotfiles/.zshrc ~/
