4a56e0cf7608190be321c9e84b6997fe5189bd58
vancanhuit <vancanhuit@gmail.com>
2020-03-22 11:52:35 +0700

    Initial commit

---
+++ scripts/setup-zsh.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo '----- Installing ohmyzsh configuration framework -----'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo '----- Installing powerlevel9k theme -----'
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+echo '----- Installing zsh syntax highlighting plugin -----'
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo '----- Installing zsh autosuggestions plugin -----'
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo '----- Installing nvm for coc.vim plugin in neovim -----'
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+
+echo '----- Installing plugin mananager for neovim -----'
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
