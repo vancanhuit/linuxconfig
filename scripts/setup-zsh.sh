6e827edf0d042eb8fb9db35c3439101f49d38bee
vancanhuit <vancanhuit@gmail.com>
2020-03-29 13:37:49 +0700

    Update

--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -12,4 +12,12 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo '----- Installing zsh autosuggestions plugin -----'
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+echo '----- Installing nvm for coc.vim plugin -----'
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+    cd "$NVM_DIR"
+      git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+      ) && \. "$NVM_DIR/nvm.sh"
+
+echo '----- Copying .zshrc -----'
 cp ../dotfiles/.zshrc ~/
