4693f63c8220108e4be84ab07d9a2eeb37194daa
vancanhuit <vancanhuit@gmail.com>
2020-03-29 13:00:32 +0700

    Update

--- scripts/setup-neovim.sh
+++ scripts/setup-neovim.sh
@@ -12,4 +12,8 @@ echo '----- Installing vim-plug -----'
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo '----- Copying init.vim ------'
+if [ ! -d "$HOME/.config/nvim" ]; then
+    mkdir -p $HOME/.config/nvim
+fi
 cp ../dotfiles/init.vim ~/.config/nvim/
