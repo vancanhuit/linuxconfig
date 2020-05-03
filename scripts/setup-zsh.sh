97d618957f8ed61e6a7c67fbb718c4033dc45a39
vancanhuit <vancanhuit@gmail.com>
2020-05-03 18:22:03 +0700

    Update fonts and scripts

--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -3,8 +3,8 @@
 echo '----- Installing ohmyzsh configuration framework -----'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-echo '----- Installing powerlevel9k theme -----'
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo '----- Installing powerlevel10k theme -----'
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo '----- Installing zsh syntax highlighting plugin -----'
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -18,6 +18,11 @@ export NVM_DIR="$HOME/.nvm" && (
     cd "$NVM_DIR"
       git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
       ) && \. "$NVM_DIR/nvm.sh"
+
+nvm install --lts
+
+echo '----- Copying .p10k.zsh -----'
+cp ../conf/.p10k.zsh $HOME
 
 echo '----- Copying .zshrc -----'
 cp ../conf/.zshrc $HOME
