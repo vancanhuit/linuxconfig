e4f6639cbcf6fae6ed3632fbcba4a57e86fb2c23
vancanhuit <vancanhuit@gmail.com>
2020-08-01 15:48:45 +0700

    Update

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -4,5 +4,5 @@ if [ ! -d "$HOME/.local/share/fonts" ]; then
     mkdir -p $HOME/.local/share/fonts
 fi
 echo '----- Copying fonts ------'
-cp ../fonts/*.ttf $HOME/.local/share/fonts/
+cp -v ../fonts/* $HOME/.local/share/fonts/
 fc-cache -fv
