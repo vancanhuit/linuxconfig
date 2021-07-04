956307d16dea47baf4b39ef5919342ee70c9bc78
vancanhuit <vancanhuit@gmail.com>
2021-07-04 19:49:45 +0700

    Update alacritty config

--- scripts/setup-allacritty.sh
+++ scripts/setup-allacritty.sh
@@ -4,4 +4,5 @@ if [ ! -d "$HOME/.config/alacritty" ]; then
     mkdir -p $HOME/.config/alacritty
 fi
 
+cp ../conf/dracula.yml $HOME/.config/alacritty/
 cp ../conf/alacritty.yml $HOME/.config/alacritty/
